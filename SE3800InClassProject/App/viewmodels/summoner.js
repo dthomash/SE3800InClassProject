93a438de10fa013541954f46bc66294a5f37a190
havemeyerd <havemeyerd@msoe.edu>
2015-02-20 02:48:51 -0600

    Added summoner level to summoner page.

--- SE3800InClassProject/App/viewmodels/summoner.js
+++ SE3800InClassProject/App/viewmodels/summoner.js
@@ -13,6 +13,7 @@
             return http.get("api/summoner", summonerName).then(function (response) {
                 that.summonerName = response.SummonerName;
                 that.summonerIconId = response.SummonerIconId;
+                that.summonerLevel = response.summonerLevel;
                 that.revisionDate = response.RevisionDate + "";
             },function(response) {
                 alert("Search failed");
